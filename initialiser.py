23ddc6d84d34b26afed16fb54f404d2650ef771e
LvanArkel <lvanarkel@hotmail.nl>
2023-03-06 14:16:15 +0100

    Also added intrinsic loader

--- initialiser.py
+++ initialiser.py
@@ -1,12 +1,21 @@
 import cv2
+import json
+import numpy as np
 
 camera_names = [f"cam{i}" for i in range(1, 5)]
 
 def load_intrinsics():
     intrinsics = []
     for camera_name in camera_names:
-        with open(f"data/{camera_name}/calibration.json"):
-            pass
+        with open(f"data/{camera_name}/calibration.json") as f:
+            calibration = json.load(f)
+            intrinsic = {
+                "ret": calibration["ret"],
+                "mtx": np.array(calibration["mtx"], dtype="float32"),
+                "dist": np.array(calibration["dist"], dtype="float32")
+            }
+            intrinsics.append(intrinsic)
+    return intrinsics
 
 def load_vid_in_directory(directory):
     vids = []
